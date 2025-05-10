4310165a2de2a5da0d41c686c1ab76acc58a5e06
mohammed0k <m.h.d-6@hotmail.com>
2025-05-10 11:16:44 +0300

    print word letter by letter

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+word = "Holberton"
+print(f"First 3 letters: {word[0:3]}")
+print(f"Last 2 letters: {word[-2:]}")
+print(f"Middle word: {word[1:-1]}")
+#print(f"First 3 letters: {word[0:3]}", "\n", f"Middle word: {word[1:-1]}", "\n", f"Last 2 letters: {word[-2:]}")
