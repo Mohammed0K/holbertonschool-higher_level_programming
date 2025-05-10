8681964422cb2082d62ccea9281409d53139062d
mohammed0k <m.h.d-6@hotmail.com>
2025-05-10 14:32:33 +0300

    fixing func print multiply

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -6,5 +6,5 @@ def fizzbuzz():
             c += "Fizz"
         if (i % 5 == 0):
             c += "Buzz"
-        print("{}".format(c), end="")
+        print("{}".format(c) or "{}".format(i), end="")
         print(" ", end="")
