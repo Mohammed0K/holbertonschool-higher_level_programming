665f343221a2d91914adc415ba48086dc9b01877
mohammed0k <m.h.d-6@hotmail.com>
2025-05-10 11:08:21 +0300

    create code print \"Welcome to Holberton School !\"

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-# str1 and str2 used
-print(f"Welcome to {str1} {str2}!")
+# Concatenate str1 and str2 with a space in between
+print(f"Welcome to {str1}"f" {str2}" "!")
