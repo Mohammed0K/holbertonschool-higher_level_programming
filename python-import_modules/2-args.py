5eb378ba7865a4407563a358f8399029a4757a91
mohammed0k <m.h.d-6@hotmail.com>
2025-05-10 15:59:15 +0300

    fix the argument

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(co))
     for i in range(co):
-        print("{}: {}".format(i + 1, sys.argv[i]))
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
