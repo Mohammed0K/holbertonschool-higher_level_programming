ea6558c1bd671c6245d57e6d41ad3c1e6d2a4950
mohammed0k <m.h.d-6@hotmail.com>
2025-05-10 14:06:17 +0300

    fixing the code

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    """Convert a string to uppercase."""
-    return str.upper()
-
-
-if __name__ == "__main__":
-    uppercases = "best"
-    uppercasese = "Best School 98 Battery street"
-    print("{}".format(uppercase(uppercases)))
-    print("{}".format(uppercase(uppercasese)))
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end="")
+    print("")
