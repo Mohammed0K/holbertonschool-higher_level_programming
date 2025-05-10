42a0334bb5487d4b8fdd2ace4363a5203ac12ec3
mohammed0k <m.h.d-6@hotmail.com>
2025-05-10 14:01:22 +0300

    create func print string in uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    """Convert a string to uppercase."""
+    return str.upper()
+
+
+if __name__ == "__main__":
+    uppercases = "best"
+    uppercasese = "Best School 98 Battery street"
+    print(f"{uppercase(uppercases)}")
+    print(f"{uppercase(uppercasese)}")
