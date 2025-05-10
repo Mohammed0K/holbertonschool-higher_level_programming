2d73a4d3fc0a9b74f81b92aa5b3d29ad354b8a11
mohammed0k <m.h.d-6@hotmail.com>
2025-05-10 16:25:09 +0300

    fix all code

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden_4 as hidden
-    list = dir(hidden)
-    for c in range(len(list)):
-        if (list[c][0] != "_"):
-            print(list[c])
+    from variable_load_5 import a
+    print("{}".format(a))
