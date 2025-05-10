716466096a0008685493ea1d3b64da0b4beb3472
mohammed0k <m.h.d-6@hotmail.com>
2025-05-10 14:49:48 +0300

    print alphabet in revers uppercase and lowercase

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-print("{}".format("".join(chr(i) if i % 2 == 0 else chr(i - 32)
-      for i in range(122, 96, -1))))
+for i in range(122, 96, -1):
+    if i % 2 == 0:
+        print("{}".format(chr(i)), end="")
+    else:
+        print("{}".format(chr(i - 32)), end="")
