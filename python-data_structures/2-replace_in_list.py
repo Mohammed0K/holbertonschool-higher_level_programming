c2d7af5cd39c0a73fd1c873429cdfb1a3d6178f4
Mohammed0K <M.H.D-6@hotmail.com>
2025-05-13 17:55:32 +0300

    fixing th code to show none in replace element

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    if idx >= 0 or idx < len(my_list):
-        my_list[idx] = element
+def replace_in_list(my_list, idx, new_element):
+    my_list[idx] = new_element
+    if idx < 0 or idx >= len(my_list):
         return None
-    return my_list
+    return my_list[idx]
