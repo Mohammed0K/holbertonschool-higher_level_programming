b7cecfc426fc12e8472c13ba64a6437fce738174
Mohammed0K <M.H.D-6@hotmail.com>
2025-05-17 19:25:57 +0300

    fix pycodestyle

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Defines a function that divides all elements of a matrix by a given number.
 """
+
+
 def matrix_divided(matrix, div):
     """Divides all elements of a matrix by a given number.
 
@@ -17,11 +19,14 @@ def matrix_divided(matrix, div):
         list of list of float: A new matrix with the results of the division.
     """
     if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
     if not all(isinstance(num, (int, float)) for row in matrix for num in row):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
     if len(matrix) == 0 or len(matrix[0]) == 0:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, (int, float)):
