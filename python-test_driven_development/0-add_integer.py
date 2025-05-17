b6cc5be16ec882f678c504d742d4ab3f64b8ccd8
Mohammed0K <M.H.D-6@hotmail.com>
2025-05-17 19:16:36 +0300

    document module

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""Defines a function that adds two integers."""
 def add_integer(a, b=98):
+    """Adds two integers.
+    Args:
+        a: first integer
+        b: second integer, defaults to 98
+    Raises:
+        TypeError: if either a or b is not an integer
+    Returns:
+        The sum of a and b, rounded down to the nearest integer.
+    """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
