3618f1b6464e9c537fbc5d9c5de7b5dbf6765b2b
Mohammed0K <M.H.D-6@hotmail.com>
2025-05-19 14:23:27 +0300

    class Square define's a square

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""lass Square that defines a square"""
+
+
+class Square:
+    """Class Square that defines a square"""
+
+    def __init__(self, size):
+        """Initialize the square with a size"""
+        self.__size = size
+        """Private instance attribute"""
