be7d567256c6dbd23e15440b215522aea3115834
Mohammed0K <M.H.D-6@hotmail.com>
2025-05-25 22:43:05 +0300

    uploud file's

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pyhthon3
+#!/usr/bin/python3
 """Rectangle class definition"""
 
 
--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pyhthon3
+#!/usr/bin/python3
 """Module for Rectangle class."""
 
 class Rectangle:
