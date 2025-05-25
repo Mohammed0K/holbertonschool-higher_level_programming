0332ed8c313b8c02996dbd516f73a86bfc873183
Mohammed0K <M.H.D-6@hotmail.com>
2025-05-25 22:52:04 +0300

    fix pycodestyle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -3,7 +3,7 @@
 
 
 class Rectangle:
-
+    """A class that defines a rectangle by its width and height."""
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle instance."""
         if not isinstance(width, int):
