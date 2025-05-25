a36cb489cdac77583f6a4feb85250f41d83b1df9
Mohammed0K <M.H.D-6@hotmail.com>
2025-05-25 14:48:05 +0300

    uploud a file

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""Module that defines a class Square."""
+
 
 class Square:
-    """Define a square by its size."""
+    """Represents a square."""
 
     def __init__(self, size=0):
-        """Initialize the square with a size."""
+        """ Initialize a square with a given size."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
@@ -13,12 +15,12 @@ class Square:
 
     @property
     def size(self):
-        """Get the size of the square."""
+        """ Get the size of the square."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Set the size of the square."""
+        """ Set the size of the square."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
