b1f0919a71bbf04e187e222a5f526284011cc53c
Mohammed0K <M.H.D-6@hotmail.com>
2025-06-01 13:27:11 +0300

    add file's

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -11,10 +11,3 @@ class Rectangle:
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-
-    def integer_validator(self, name, value):
-        """Validate that value is a positive integer."""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
