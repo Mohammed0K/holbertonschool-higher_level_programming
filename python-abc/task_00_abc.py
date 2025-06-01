38d12483c910df7f97f86f21b2134dd09b98772c
Mohammed0K <M.H.D-6@hotmail.com>
2025-06-01 16:57:39 +0300

    test file

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+""""define the Animal class and its subclasses abc"""
+
+
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    """Abstract base class for all animals"""
+
+    @abstractmethod
+    def sound(self) -> str:
+        """Return the sound made by the animal"""
+        pass
