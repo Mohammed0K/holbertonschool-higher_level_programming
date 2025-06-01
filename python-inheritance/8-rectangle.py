e0d7aa9062c207f55d6746954a1ca1f3e3a9b6e7
Mohammed0K <M.H.D-6@hotmail.com>
2025-06-01 13:25:16 +0300

    add file's

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""Module that defines a class Rectangle."""
+
+
+class Rectangle:
+    """Class that defines a rectangle with width and height."""
+
+    def __init__(self, width, height):
+        """Initialize the rectangle with width and height."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def integer_validator(self, name, value):
+        """Validate that value is a positive integer."""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
