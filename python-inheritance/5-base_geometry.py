8189b77539ed237b473186e8524ddb7ffe3565e4
Mohammed0K <M.H.D-6@hotmail.com>
2025-06-01 12:44:34 +0300

    add file and fix code

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Module that defines a class MyList that inherits from list."""
+
+
+#!/usr/bin/python3
+"""Module that defines a class MyList that inherits from list."""
+
+
+class BaseGeometry:
+    """A class that defines basic geometric operations."""
+    pass
--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,7 +1,1 @@
 #!/usr/bin/python3
-"""Module that defines a class MyList that inherits from list."""
-
-
-class BaseGeometry:
-    """Class that defines a base geometry."""
-    pass
