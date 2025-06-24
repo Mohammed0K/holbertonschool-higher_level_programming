d78bc58e6dcf0c0944fd134eae90de73dbf73006
Mohammed0K <M.H.D-6@hotmail.com>
2025-06-24 11:23:24 +0300

    commit

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,2 @@
 -- script that creates a table called first_table in the current database in your MySQL server.
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    age INT NOT NULL
-);
+SHOW first_table;
