0a2e5377b1f34f4aff67b3533cec9ffdd513b59e
Mohammed Abdullah <81221967+Mohammed0K@users.noreply.github.com>
2025-06-25 11:58:45 +0300

    Update 9-full_creation.sql

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -5,9 +5,8 @@ CREATE TABLE IF NOT EXISTS second_table(
   score INT
 );
 
-INSERT INTO second_table(id, name, score) VALUE (
-   1, "John",  10),
-  (2, "Alex", 3),
-  (3, "Bob", 1)
-  (4, "George", 8
-);
+INSERT INTO second_table(id, name, score) VALUE 
+  (1,"John",10),
+  (2,"Alex",3),
+  (3,"Bob",1)
+  (4,"George",8);
